e09087f39002ba6a27a02d3930e6c5136757c284
Paweu <paloboo@o2.pl>
2021-01-06 01:17:46 +0100

    added icons, and styling to singletodo

--- src/components/todolist/SingleTodo.js
+++ src/components/todolist/SingleTodo.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import firebase from "../../firebase";
+import "./SingleTodo.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 const SingleTodo = (props) => {
-  function handleClick() {
+  const [doneClass, setDoneClass] = useState("false");
+
+  function handleDelete() {
     firebase
       .firestore()
       .collection("todos")
@@ -12,10 +17,22 @@ const SingleTodo = (props) => {
         console.log(props.id, "got deleted");
       });
   }
+
+  function handleDone() {
+    setDoneClass(!doneClass);
+  }
   return (
-    <ul>
-      <li onClick={handleClick}>
-        {props.name}, {props.task}
+    <ul className="SingleTodo_wrapper">
+      {/* do zrobienia - wrzucenie do bazy danych info czy task jest zrobiony czy nie.  */}
+      <li className={doneClass ? "SingleTodo" : "SingleTodo SingleTodo_done"}>
+        <span className="SingleTodo_name">{props.name}</span>{" "}
+        <span className="SingleTodo_task">{props.task}</span>
+        <FontAwesomeIcon icon={faCheck} onClick={handleDone} />
+        <FontAwesomeIcon
+          className="SingleTodo_icon"
+          icon={faTrashAlt}
+          onClick={handleDelete}
+        />
       </li>
     </ul>
   );
