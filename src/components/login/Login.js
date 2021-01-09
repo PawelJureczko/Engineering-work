bc07040271a8a1bb73d79512239b021b4240b541
Paweu <paloboo@o2.pl>
2021-01-09 11:05:38 +0100

    added hooks and methods to login component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,18 +6,82 @@ import Container from "react-bootstrap/Container";
 import "./Login.scss";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import firebase from "../../firebase";
 
 const Login = () => {
-  function handleSubmit(e) {
-    console.log(email);
-    e.preventDefault();
-  }
-
-  function handleChange(e) {
-    setEmail(e.target.value);
-  }
-
+  const [user, setUser] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState("");
+
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
+
+  const handleLogin = () => {
+    clearErrors();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleSignup = () => {
+    clearErrors();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+  };
+
+  const authListener = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
+
   return (
     <Container>
       <Form>
@@ -26,7 +90,7 @@ const Login = () => {
           <Form.Control
             type="email"
             placeholder="Enter email"
-            onChange={handleChange}
+            // onChange={handleChange}
           />
         </Form.Group>
 
@@ -39,7 +103,7 @@ const Login = () => {
             <Button
               variant="primary"
               type="submit"
-              onSubmit={handleSubmit}
+              // onSubmit={handleSubmit}
               value={email}
             >
               Submit
